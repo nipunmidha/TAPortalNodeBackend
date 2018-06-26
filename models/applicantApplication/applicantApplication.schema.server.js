e6c3b5777ba5d49e3c43b7b17ba20c1b8e97bc19
Nipun Midha <nipunmidha@ccs.neu.edu>
2018-06-26 15:33:02 -0400

    saving

--- models/applicantApplication/applicantApplication.schema.server.js
+++ models/applicantApplication/applicantApplication.schema.server.js
@@ -3,7 +3,7 @@ const RATING=[1,2,3,4,5]
 var applicantApplication = mongoose.Schema({
         isSelected:  { type: Boolean, default: false },
         instructorRemarks: String,
-        instructorRating:{type:Number,enum:RATING},
+            instructorRating:{type:Number,enum:RATING},
         applicant: {type: mongoose.Schema.Types.ObjectId, ref: 'ApplicantModel'},
         position: {type: mongoose.Schema.Types.ObjectId, ref: 'InstructorCourseModel' }
     },
